ee693263d28ae3975a4829e07b75de492afccd68
test <lan@shdeMac-mini.local>
2016-09-28 15:29:56 +0800

    新建项目

---
+++ ProjectB/ProjectB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProjectB
+//
+//  Created by lanou on 2016/9/28.
+//  Copyright © 2016年 lanou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProjectB/ProjectB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProjectB
+//
+//  Created by lanou on 2016/9/28.
+//  Copyright © 2016年 lanou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
