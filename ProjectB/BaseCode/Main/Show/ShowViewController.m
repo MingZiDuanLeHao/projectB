967643da38dc935e70539de034abf4dc03919eb4
test <lan@shdeMac-mini.local>
2016-09-30 17:58:51 +0800

    gvfgv

--- ProjectB/BaseCode/Main/Show/ShowViewController.m
+++ ProjectB/BaseCode/Main/Show/ShowViewController.m
@@ -41,15 +41,46 @@
 
 //进入视频列表
 - (IBAction)vedioHandle:(UIButton *)sender {
+
+    
+
+      [self animationWithView:self.view WithAnimationTransition:UIViewAnimationTransitionCurlDown];
     VedioMainController *vedioVC = [VedioMainController new];
-    [self.navigationController pushViewController:vedioVC animated:YES];
+    //[self.navigationController pushViewController:vedioVC animated:YES];
     
 }
 - (IBAction)RadioHandle:(UIButton *)sender {
     RadioMainController *radioVC = [RadioMainController new];
     [self.navigationController pushViewController:radioVC animated:YES];
 }
-
+- (void) animationWithView : (UIView *)view WithAnimationTransition : (UIViewAnimationTransition) transition
+{
+    [UIView animateWithDuration:1 animations:^{
+        [UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
+        [UIView setAnimationTransition:transition forView:view cache:YES];
+    }];
+}
+- (void) transitionWithType:(NSString *) type WithSubtype:(NSString *) subtype ForView : (UIView *) view
+{
+    //创建CATransition对象
+    CATransition *animation = [CATransition animation];
+    
+    //设置运动时间
+    animation.duration = 1;
+    
+    //设置运动type
+    animation.type = type;
+    if (subtype != nil) {
+        
+        //设置子类
+        animation.subtype = subtype;
+    }
+    
+    //设置运动速度
+    animation.timingFunction = UIViewAnimationOptionCurveEaseInOut;
+    
+    [view.layer addAnimation:animation forKey:@"animation"];
+}
 
 
 /*
