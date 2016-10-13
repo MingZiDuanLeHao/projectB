2b479fa217c3f6b88d26f204ce7b251c83193541
pingzimon <lan@pingzideMac-mini.local>
2016-10-13 14:14:24 +0800

    dasfdad

--- ProjectB/BaseCode/RightWeather/RightWeatherController.m
+++ ProjectB/BaseCode/RightWeather/RightWeatherController.m
@@ -7,8 +7,25 @@
 //
 
 #import "RightWeatherController.h"
+#import "ApiStoreSDK.h"
+#import "WeatherDataModels.h"
 
 @interface RightWeatherController ()
+@property (weak, nonatomic) IBOutlet UILabel *temLabel;
+@property (weak, nonatomic) IBOutlet UILabel *locLabel;
+@property (weak, nonatomic) IBOutlet UILabel *timeLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *coverImg;
+@property (weak, nonatomic) IBOutlet UILabel *stateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *HighLowTemLabel;
+@property (weak, nonatomic) IBOutlet UILabel *PMLabel;
+@property (weak, nonatomic) IBOutlet UILabel *windLabel;
+@property (weak, nonatomic) IBOutlet UICollectionView *sevenDayCollView;
+@property (weak, nonatomic) IBOutlet UILabel *wearingLabel;
+@property (weak, nonatomic) IBOutlet UILabel *wearingDetailLabel;
+@property (weak, nonatomic) IBOutlet UILabel *coughLabel;
+@property (weak, nonatomic) IBOutlet UILabel *coughDetailLabel;
+
+@property(nonatomic,strong)WeatherBaseClass *base;
 
 @end
 
@@ -17,6 +34,61 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    //1.定位
+    //2.请求数据显示UI
+//    [self requestData];
+}
+
+
+-(void)requestData
+{
+    
+    NSString *city = @"guangzhou" ;
+    NSString *apikey = @"c184575a002af5fb6dee57adc1cca85c";
+    
+    //实例化一个回调，处理请求的返回值
+    APISCallBack* callBack = [APISCallBack alloc];
+    
+    callBack.onSuccess = ^(long status, NSString* responseString) {
+        if(responseString != nil) {
+            NSDictionary *dic = [self dictionaryWithJsonString:responseString];
+            _base = [WeatherBaseClass modelObjectWithDictionary:dic];
+        }
+    };
+    
+    callBack.onError = ^(long status, NSString* responseString) {
+        NSLog(@"onError");
+        
+    };
+   
+    
+    //部分参数
+    NSString *uri = @"http://apis.baidu.com/heweather/weather/free";
+    NSString *method = @"post";
+    NSMutableDictionary *parameter = [[NSMutableDictionary alloc] init];
+    [parameter setObject:city forKey:@"city"];
+    
+    //请求API
+    [ApiStoreSDK executeWithURL:uri method:method apikey:apikey parameter:parameter callBack:callBack];
+
+}
+
+- (NSDictionary *)dictionaryWithJsonString:(NSString *)jsonString {
+    if (jsonString == nil) {
+        return nil;
+    }
+    
+    NSData *jsonData = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
+    NSError *err;
+    NSDictionary *dic = [NSJSONSerialization JSONObjectWithData:jsonData
+                                                        options:NSJSONReadingMutableContainers
+                                                          error:&err];
+    if(err) {
+        NSLog(@"json解析失败：%@",err);
+        return nil;
+    }
+    return dic;
 }
 
 - (void)didReceiveMemoryWarning {
